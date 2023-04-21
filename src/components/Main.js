b6de18a042a67643842d8bc5a682eca175a1cf1f
Raul Zarate <101022036+zaratr@users.noreply.github.com>
2023-04-20 21:15:20 -0700

    added some fixes and code now runs

--- src/components/Main.js
+++ src/components/Main.js
@@ -21,24 +21,23 @@ class Main extends React.Component {
   }
 
   handleCityInput = (ev) => {
+    console.log("city: " ,ev.target.value)
     this.setState({
       city: ev.target.value
     })
   }
 
 
-  getCityData = async (ev) => {
-    ev.preventDefault();
+  getCityData = async (e) => {
+    e.preventDefault();
     try {
       let url = `https://us1.locationiq.com/v1/search?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&q=${this.state.city}&format=json`
 
       let cityData = await axios.get(url)
       
-      console.log(cityData.data)
 
       let mapUrl = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_LOCATION_IQ_API_KEY}&center=${cityData.data[0].lat},${cityData.data[0].lon}&zoom=14`
-
-      let letResult = await axios.get(mapUrl)
+      //let letResult = await axios.get(mapUrl)
       // console.log('this is the', letResults.config.url)
       // this.setState({ map: letResults.config.url })
       this.setState({ map: mapUrl })
@@ -67,20 +66,14 @@ class Main extends React.Component {
     let url = `${process.env.REACT_APP_SERVER}/weather?searchQuery=${this.state.city}`
 
     let res = await axios.get(url)
-    console.log(res.data)
-    console.log('HERE')
     this.setState({
       forecast: res.data
-    
     })
     } catch(error){
       console.error(error.message)
-      console.error(error.captureStackTrace())
-      // TODO: set state with the error boolean and error message
-      // this.setState({
-      // })
     }
   }
+
 
   render() {
     return (
@@ -88,7 +81,7 @@ class Main extends React.Component {
         <h2>City Data</h2>
         <form onSubmit={this.getCityData}>
           <label> Enter in a City name:
-            <input type="text" onInput={this.handleCityInput} />
+            <input type="text" onChange={this.handleCityInput} />
           </label>
           <Button type="submit">Explore!</Button>
         </form>
@@ -98,16 +91,19 @@ class Main extends React.Component {
             ? <p>{this.state.errorMsg}</p>
             : <p>{this.state.cityData.display_name}</p>
         }
-        <Card>
-          <Card.Text>
-            <ul>
-              <li>City: {this.state.cityData.display_name}</li>
-              <li>Longitude: {this.state.cityData.lon}</li>
-              <li>Latitude: {this.state.cityData.lat}</li>
-              <li>Map: <img src={this.state.map} alt="City Map" /> </li>
-            </ul>
-          </Card.Text>
-        </Card>
+        {this.state.cityData ? 
+          <div>
+              <ul>
+                <li>City: {this.state.cityData.display_name}</li>
+                <li>Longitude: {this.state.cityData.lon}</li>
+                <li>Latitude: {this.state.cityData.lat}</li>
+                <li>
+
+                <img src={this.state.map} alt="City Map" /> 
+                </li>
+              </ul>
+          </div>
+        :null}
       </>
     )
   }
